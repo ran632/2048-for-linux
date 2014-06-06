22258958bccacdec1dace085d1908e1bce6c2a45
ran632 <credential.helper>
2014-06-06 20:29:39 +0300

    LEFT

--- 2048.c
+++ 2048.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <curses.h>
+
+
 
 #define SIZE 4
 #define PRIME 2
@@ -12,17 +15,58 @@ void placeNum();
 void printBoard();
 
 int board[SIZE][SIZE];
+WINDOW * mainwin;
 
+int main(){
 
-int main(int argc, int* argv[]){
-	int i=0;
+  	
+	int i=0/*ROW*/, j=0/*COL*/, k;
 	srand(time(NULL));
 	initialize();
+
 	while(1){
 		placeNum();
+		clear();
+		refresh();
 		printBoard();
-		break;
+		int keyPress = getch();
+		switch(keyPress){
+			case KEY_RIGHT:
+
+				break;
+			case KEY_LEFT:
+				
+				for (i = 0; i < SIZE; i++){
+					int temp[]= {0,0,0,0};
+					k=0;
+					for (j = 0; j < SIZE; j++){
+						if (board[i][j] == EMPTY)
+							continue;
+						if (temp[k] == 0)
+							temp[k] = board[i][j];
+						else if(temp[k] == board[i][j]){
+							temp[k] = temp[k]*PRIME;
+							k++;
+						}
+						else{
+							k++;
+							temp[k] = board[i][j];
+						}
+					}
+					for(k = 0 ; k < SIZE ; k++)
+						board[i][k] = temp[k];
+				}
+				break;
+			case KEY_UP:
+				break;
+			case KEY_DOWN:
+				break;
+		}
 	}
+
+    getch();
+	endwin();
+	return 0;
 }
 
 
@@ -33,6 +77,11 @@ void initialize(){
 			board[i][j] = EMPTY;
 		}
 	}
+
+	mainwin = initscr();
+	noecho();
+  	keypad(mainwin, TRUE);
+
 }
 
 int ran(){
